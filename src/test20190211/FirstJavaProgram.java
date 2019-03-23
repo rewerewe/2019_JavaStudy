ddc5214b84b1b86ca22dfc8df4c158c1c6b935d0
Imme <rewerewe113@gmail.com>
2019-03-23 17:26:51 +0900

    EUC-KR로 전환

--- src/test20190211/FirstJavaProgram.java
+++ src/test20190211/FirstJavaProgram.java
@@ -4,7 +4,7 @@ public class FirstJavaProgram
 {
 	public static void main(String[] args)
 	{
-		// 이메 테스트
+		// 이메
 		System.out.println("Welcome to Java");
 		System.out.println("First Java Program");
 	}	
